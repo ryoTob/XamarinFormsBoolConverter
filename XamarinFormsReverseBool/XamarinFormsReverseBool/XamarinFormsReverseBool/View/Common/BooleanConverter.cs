1d4b173203a1661c7e0cd87f64f327b6292fc8ba
ryoTob <accelerator.pro36@gmail.com>
2017-03-01 02:33:34 +0900

    Initialize
    Initialize

---
+++ XamarinFormsReverseBool/XamarinFormsReverseBool/XamarinFormsReverseBool/View/Common/BooleanConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XamarinFormsReverseBool.View.Common
+{
+    class BooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+    }
+}
---
+++ XamarinFormsReverseBool/XamarinFormsReverseBool/XamarinFormsReverseBool/View/MainView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace XamarinFormsReverseBool.View
+{
+    public partial class MainView : ContentPage
+    {
+        private bool _VisibleText;
+        public bool VisibleText
+        {
+            get { return _VisibleText; }
+            set
+            {
+                _VisibleText = value;
+                OnPropertyChanged("VisibleText");
+            }
+        }
+
+        public MainView()
+        {
+            InitializeComponent();
+            VisibleText = true;
+            BindingContext = this;
+        }
+
+        public void OnButtonClicked(object sender, EventArgs args)
+        {
+            VisibleText = !VisibleText;
+        }
+    }
+}
